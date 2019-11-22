e3c769532993798f92564a9fa8f77bb5064c89a0
Okjuszide <44412083+Okjuszide@users.noreply.github.com>
2019-11-22 21:07:49 +0100

    Adding components

---
+++ Components/Section.js
@@ -0,0 +1,14 @@
+import Title from "./Title"
+
+function Section(props) {
+    return (
+        <section className="container">
+            <Title title={props.title}/>
+            <div className="content">
+                {props.children}
+            </div>
+        </section>
+    )
+}
+
+export default Section
